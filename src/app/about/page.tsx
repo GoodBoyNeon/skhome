0602ddbb95e4f447636cf1e3e155925262ceb798
GoodBoyNeon <goodboyneon03@gmail.com>
2024-04-28 20:48:45 +0545

    feat: footer

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+import AboutCard from "@/components/AboutCard";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="m-4">
+      <div className="mb-4">
+        <h1 className="font-black text-4xl">Welcome to S.K. Home Traders.</h1>
+      </div>
+
+      <p className="text-base">
+        We have been providing quality services to our customers since 2016.
+        That&apos;s 8 years of excellent trust between us and our customers.
+      </p>
+
+      <span className="h-24 flex w-screen"></span>
+
+      <h2 className="font-bold text-3xl text-center m-3">Why Us?</h2>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <div className="m-4 text-center font-semibold text-lg">
-      <h2>S.K. Home Traders</h2>
+    <div className="flex flex-col gap-3 items-center p-4 bg-[#f3f4f6]">
+      <div className="border-b">
+        <h2 className="text-center font-semibold text-lg mb-1">
+          S.K. Home Traders
+        </h2>
+        <p className="text-muted-foreground mb-3">
+          Your go-to store for home appliances in Kathmandu Valley. Visit our
+          physical store located at Madhyapur Thimi, Bhaktapur.{" "}
+          <Link
+            href={"https://maps.app.goo.gl/9EeqihMRFqi5wjQ16"}
+            className="hover:underline text-blue-400"
+          >
+            Check on Google Maps
+          </Link>
+        </p>
+      </div>
+
+      <div className="text-2xl flex gap-3">
+        <Link href={"https://facebook.com"}>
+          <FaFacebook />
+        </Link>
+
+        <Link href={"https://instagram.com"}>
+          <FaInstagram />
+        </Link>
+      </div>
     </div>
   );
 }
