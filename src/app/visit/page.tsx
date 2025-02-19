f87725af33ceca4a412bd5bd70fcd5d3030998ce
GoodBoyNeon <goodboyneon03@gmail.com>
2025-02-19 06:01:59 +0000

    loading page

---
+++ src/app/visit/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Visit = () => {
+  return (
+    <>
+      <iframe
+        width="450"
+        height="250"
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_API_KEY}&q=S.K Home Traders`}
+      >
+      </iframe></>
+  )
+}
+
+export default Visit;
