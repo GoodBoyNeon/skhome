0256391f49bf3084449cf289a301fb52c942f61c
GoodBoyNeon <goodboyneon03@gmail.com>
2025-05-15 21:40:45 +0545

    chore(eslint): add tanstack-query linter plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
+  ...pluginQuery.configs["flat/recommended"],
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
