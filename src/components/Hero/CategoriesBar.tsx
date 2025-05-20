3122640f346ab6b42522b8fad9b2c8c2313d0e80
GoodBoyNeon <goodboyneon03@gmail.com>
2025-05-20 21:33:13 +0545

    fix: styling issue with scrollable category bar

--- src/components/Hero/CategoriesBar.tsx
+++ src/components/Hero/CategoriesBar.tsx
@@ -8,27 +8,31 @@ export default async function Categories() {
   );
 
   return (
-    <div className="bg-background m-2 flex justify-center gap-8 overflow-x-auto">
-      {categories.map((category) => (
-        <div key={category.id} className="max-w-xl text-center">
-          <Link
-            prefetch
-            href={`category/${category.urlSlug}`}
-            className="hover:bg-accent flex flex-col items-center justify-center rounded p-2"
-          >
-            {category.image != "" && (
-              <Image
-                src={category.image}
-                alt={`${category.name}-image`}
-                className="object-cover"
-                width={60}
-                height={60}
-              />
-            )}
-            <h3 className="text-sm font-normal">{category.name}</h3>
-          </Link>
+    <div className="bg-background overflow-x-scroll">
+      <div className="m-2 flex justify-center">
+        <div className="flex gap-3">
+          {categories.map((category) => (
+            <div key={category.id} className="max-w-xl shrink-0 text-center">
+              <Link
+                prefetch
+                href={`category/${category.urlSlug}`}
+                className="hover:bg-accent flex flex-col items-center justify-center rounded p-2"
+              >
+                {category.image != "" && (
+                  <Image
+                    src={category.image}
+                    alt={`${category.name}-image`}
+                    className="object-cover"
+                    width={60}
+                    height={60}
+                  />
+                )}
+                <h3 className="text-sm font-normal">{category.name}</h3>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
